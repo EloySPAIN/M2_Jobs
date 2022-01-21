b2f975dd11dfbfb799874eac37fd2bd21b1cc8af
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-01-21 10:38:02 +0100

    milestone1 corregido

--- src/packageM02/clases/mainClass.java
+++ src/packageM02/clases/mainClass.java
@@ -15,9 +15,7 @@ public class mainClass {
 			do {
 				System.out.println("Indique Nombre: ");
 				String name = ask.next();
-				System.out.println("Indique Salario: ");
-				double salary = ask.nextDouble();
-				despliegaOpcion(name, salary);
+				despliegaOpcion(name);
 				System.out.println("Marque 1 para iniciar el cálculo de sueldo empleado:");
 				opcion = ask.nextInt();
 			}while(opcion == 1);
@@ -28,21 +26,27 @@ public class mainClass {
 		ask.close();
 	}
 	
-	public static void despliegaOpcion(String name, double salary) {
+	public static void despliegaOpcion(String name) {
 		Scanner ask = new Scanner(System.in);
 		System.out.println("Indique opción : \n1.- Marque 1 para calcular sueldo empleado: \n2.- Marque 2 para calcular sueldo Manager: \n3.- Marque 3 para calcular sueldo Jefe: \n4.- Marque 3 para calcular sueldo voluntario:");
 		int respuesta = ask.nextInt();
 		switch(respuesta) {
 			case 1:
+				System.out.println("Indique Salario: ");
+				double salary = ask.nextDouble();
 				Employee employee = new Employee(name, salary);
 				System.out.println(employee.toString());
 				break;
 			case 2:
-				Manager manager = new Manager(name, salary);
+				System.out.println("Indique Salario: ");
+				double salary1 = ask.nextDouble();
+				Manager manager = new Manager(name, salary1);
 				System.out.println(manager.toString());
 				break;
 			case 3:
-				Boss boss = new Boss(name, salary);
+				System.out.println("Indique Salario: ");
+				double salary2 = ask.nextDouble();
+				Boss boss = new Boss(name, salary2);
 				System.out.println(boss.toString());
 				break;
 			case 4:
